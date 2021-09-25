56bcceb74896483d6470ab55b68ee545cc5492b7
Isidoro de la Vieja <felixsilvestrix89@gmail.com>
2021-09-25 19:43:25 +0200

    status and message optionals

--- Directives/CheckStatus.php
+++ Directives/CheckStatus.php
@@ -1,8 +1,14 @@
 <?php /*dlv-code-engine***/
 
 if ( $state->message()->getStatus() >= 400) {
-    $state->memory()->set('error.status', '502');
-    $state->memory()->set('error.message', 'Server Side Error');
+    $status = isset($config['status']) 
+        ? $config['status'] 
+        : 502;
+    $message = isset($config['message']) 
+        ? $config['message'] 
+        : 'Server Side Error';
+    $state->memory()->set('error.status', $status);
+    $state->memory()->set('error.message', $message);
     throw new \Exception('Request to '.
             $state->memory()->get('last-http-request')
             .' returned '.$state->message()->getStatus()
