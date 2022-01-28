d04624aa6acfb7bc057a3fb8b3e2a3e334402539
Isidoro de la Vieja <felixsilvestrix89@gmail.com>
2022-01-28 21:08:03 +0100

    pull can cache

--- Directives/Pull.php
+++ Directives/Pull.php
@@ -2,6 +2,7 @@
 
 $verb = 'GET';
 $body = '';
+$keyCache = null;
 if (isset($config['body'])) {
     $verb = 'POST';
     $body = $config['body'];
@@ -14,6 +15,23 @@ ModifyMessage::run($state,[
     'queryParams' => isset($config['queryParams']) ?$config['queryParams'] : [],
     'body' => $body
 ]);
+
+if (isset($config['cache'])) {
+    $keyCache = 'pull:cache:'.md5($config['host'].serialize($state->message()));
+    $state->memory()->set('pullCache',null);
+    RedisGet::run($state,[
+        'key' => $keyCache,
+        'target' => 'pullCache'
+    ]);
+    $pullCache = $state->memory()->get('pullCache');
+    if ($pullCache) {
+        LoadModel::run($state,[
+            'source' => $pullCache['data'],
+            'target' => $config['target']
+        ]);
+        return;
+    }
+}
 
 Http::run($state,[
     'host' => $config['host']
@@ -32,5 +50,13 @@ if ( ($verify || $verifyModel) && $state->message()->getStatus() < 299 ) {
         'source' => $state->message()->getBodyAsArray(),
         'target' => $config['target'],
         'server-side-error' => true
+    ]);
+}
+
+if ($keyCache) {
+    RedisSet::run($state,[
+        'key' => $keyCache,
+        'value' => [ 'data' => $state->message()->getBodyAsArray() ],
+        'expire' => $config['cache']
     ]);
 }
