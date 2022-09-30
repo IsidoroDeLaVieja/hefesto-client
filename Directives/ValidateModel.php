72fadb5774d46812659fcbd19c1a1b4a9156fec9
Isidoro de la Vieja <felixsilvestrix89@gmail.com>
2022-09-30 21:46:54 +0200

    upgrade validate model

--- Directives/ValidateModel.php
+++ Directives/ValidateModel.php
@@ -3,29 +3,22 @@
 $schema = json_encode($state->map($config['name'])->read());
 $validator = new \Opis\JsonSchema\Validator();
 
-$data = json_decode(json_encode($state->memory()->get($config['name'])));
+$data = json_decode(json_encode($state->memory()->get($config['name'])));//array to object
 $result = $validator->validate($data, $schema);
 
-if ( ! $result->isValid() ) {
-    if (isset($config['server-side-error']) && $config['server-side-error'] === true) {
-        $state->memory()->set('error.status', '502');
-        $state->memory()->set('error.message', 'Server Side Error');
-        throw new \Exception('Server Side Error');
-    }
-    
-    $error = $result->getFirstError();
-    
-    $wrongData = $error->dataPointer();
-    $wrongData = $wrongData ? $wrongData[0] : '';
-    $message = $wrongData.' '.$error->keyword().' ';
-    
-    $keywordArgs = $error->keywordArgs();
-    foreach($keywordArgs as $key => $value) {
-        $value = is_array($value) ? implode(' ',$value) : $value;
-        $message .= $key.' '.$value.' ';
-    }
-    
-    $state->memory()->set('error.status', '400');
-    $state->memory()->set('error.message', trim($message));
-    throw new \Exception(trim($message));
+if ( $result->isValid() ) {
+    return;
 }
+
+if (isset($config['server-side-error']) && $config['server-side-error'] === true) {
+    $state->memory()->set('error.status', 502);
+    $state->memory()->set('error.message', 'Server Side Error');
+    throw new \Exception('Server Side Error');
+}
+
+$formatter = new \Opis\JsonSchema\Errors\ErrorFormatter();
+$message = $formatter->formatErrorMessage($result->error());
+
+$state->memory()->set('error.status', 400);
+$state->memory()->set('error.message', $message);
+throw new \Exception($message);
